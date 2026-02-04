130f76aa9ed4a4351b03f25572a446daf9a553a7
Stipe Bošnjak <stipebosnjak05@gmail.com>
2026-02-04 17:11:42 +0100

    feat: setup task schema

--- mobile/db/schema/task.schema.ts
+++ mobile/db/schema/task.schema.ts
@@ -1,11 +1,36 @@
-import { sqliteTable as table } from "drizzle-orm/sqlite-core";
 import * as t from "drizzle-orm/sqlite-core";
-import { AnySQLiteColumn } from "drizzle-orm/sqlite-core";
-import { text, sqliteTable } from "drizzle-orm/sqlite-core";
 import { createId } from "@paralleldrive/cuid2";
+import { TaskStatus } from "@/types/create.types";
+import { InferSelectModel, InferInsertModel } from "drizzle-orm";
 
-export const tasks = table("tasks", {
-  id: text("id")
+export const tasks = t.sqliteTable("tasks", {
+  id: t
+    .text("id")
     .primaryKey()
     .$defaultFn(() => createId()),
+  title: t.text("title").notNull(),
+  created_at: t
+    .integer("created_at", { mode: "timestamp_ms" })
+    .notNull()
+    .$defaultFn(() => new Date()),
+  updated_at: t
+    .integer("updated_at", { mode: "timestamp_ms" })
+    .$onUpdateFn(() => new Date()),
+  completed_at: t.integer("completed_at", { mode: "timestamp_ms" }),
+  is_deleted: t
+    .integer("is_deleted", { mode: "boolean" })
+    .notNull()
+    .default(false),
+  status: t.text("status").$type<TaskStatus>(),
+  project_id: t.text("project_id"),
+  section_id: t.text("section_id"),
+  area_id: t.text("area_id"),
+  start_date: t.integer("start_date"),
+  due_date: t.integer("due_date"),
+  sort_order: t.real("sort_order").notNull(),
+  duration_min: t.integer("duration_min"),
+  recurrence_rule: t.text("recurrence_rule"),
 });
+
+export type Task = InferSelectModel<typeof tasks>;
+export type NewTask = InferInsertModel<typeof tasks>;
