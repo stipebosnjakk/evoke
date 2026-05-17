05e696a8140d6a48769fbbf1b5dcdba16b200091
Stipe Bošnjak <stipebosnjak05@gmail.com>
2026-05-17 23:29:10 +0200

    refactor: update project schema

---
+++ mobile/db/schemas/project.schema.ts
@@ -0,0 +1,25 @@
+import * as t from "drizzle-orm/sqlite-core";
+import { createId } from "@paralleldrive/cuid2";
+
+import { ProjectStatus } from "@/types/project.types";
+import { InferInsertModel, InferSelectModel } from "drizzle-orm";
+
+export const projects = t.sqliteTable("projects", {
+  id: t
+    .text("id")
+    .primaryKey()
+    .$defaultFn(() => createId()),
+  name: t.text("name"),
+  color: t.text("color"),
+  status: t.text("status").$type<ProjectStatus>().notNull().default("active"),
+  completed_at: t.integer("completed_at"),
+  archived_at: t.integer("archived_at"),
+  created_at: t
+    .integer("created_at")
+    .notNull()
+    .$defaultFn(() => Date.now()),
+  updated_at: t.integer("updated_at"),
+});
+
+export type Project = InferSelectModel<typeof projects>;
+export type NewProject = InferInsertModel<typeof projects>;
